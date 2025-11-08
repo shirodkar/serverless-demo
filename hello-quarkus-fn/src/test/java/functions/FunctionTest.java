a17dd6ef81be529baab0e7e35c991e20ee42b3ee
Sachin Shirodkar <sshirodk@redhat.com>
2025-11-08 14:50:13 -0500

    clean up

--- hello-quarkus-fn/src/test/java/functions/FunctionTest.java
+++ hello-quarkus-fn/src/test/java/functions/FunctionTest.java
@@ -2,12 +2,10 @@ package functions;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 
 @QuarkusTest
 public class FunctionTest {
