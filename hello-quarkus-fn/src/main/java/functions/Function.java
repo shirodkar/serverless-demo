4573f5fea9c8949f8e5d18118ce192ce9d0971f8
Sachin Shirodkar <sshirodk@redhat.com>
2025-11-09 17:10:04 -0500

    add db integration to function

--- hello-quarkus-fn/src/main/java/functions/Function.java
+++ hello-quarkus-fn/src/main/java/functions/Function.java
@@ -1,23 +1,45 @@
 package functions;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import io.quarkus.funqy.Funq;
+import jakarta.inject.Inject;
 
 /**
  * Your Function class
  */
 public class Function {
 
+    @Inject
+    DataSource dataSource;
+
     /**
      * Use the Quarkus Funqy extension for our function. This function simply echoes its input
      * @param input a Java bean
      * @return a Java bean
+     * @throws SQLException 
      */
     @Funq
-    public Output function(Input input) {
+    public Output function(Input input) throws SQLException {
 
-        // Add business logic here
+        List<String> results = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement("SELECT name FROM customer")) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    results.add(rs.getString("name"));
+                }
+            }
+        }
 
-        return new Output(input.getMessage());
+        return new Output(results.toString());
     }
 
 }
